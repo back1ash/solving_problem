9255ff5a47f7feca14ba343bda8a37c2e692164e
Lee haejun <teamforhj@gmail.com>
2024-12-23 23:24:17 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+tmp = input()
+if tmp == "SONGDO":
+    print("HIGHSCHOOL")
+elif tmp == "CODE":
+    print("MASTER")
+elif tmp == "2023":
+    print("0611")
+elif tmp == "ALGORITHM":
+    print("CONTEST")
